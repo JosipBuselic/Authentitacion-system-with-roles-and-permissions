d4de1b1dc53f7414c7577e4f169fc365623db0b4
JosipBuselic <josip.busilica@gmail.com>
2025-07-28 01:34:19 +0200

    commit

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
