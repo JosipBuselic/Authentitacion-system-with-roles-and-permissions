1e709217e746632a785935d787e2c8596898a384
JosipBuselic <josip.busilica@gmail.com>
2025-07-29 11:55:01 +0200

    .

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -62,7 +62,7 @@ export default function Dashboard() {
         <div className="mt-10">
           <button
             onClick={handleLogout}
-            className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
+            className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition cursor-pointer"
           >
             Logout
           </button>
